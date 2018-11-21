2d8d3cdc6c14fe0d38196b3189942c557ddf38b1
pasthortown <pastorchessmaster@gmail.com>
2018-11-21 16:05:15 -0500

    ok

--- angular1/src/app/body/ej1/ej1.component.ts
+++ angular1/src/app/body/ej1/ej1.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ej1.component.css']
 })
 export class Ej1Component implements OnInit {
+  numerosPrimos = [];
+  numero = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  primos(numero) {
+  for (let i = 1; i <= numero; i++) {
+    if (this.verificarPrimo(i)) {
+      console.log(i);
+    }
+  }
+  }
+  verificarPrimo(i) {
+    for ( let x = 1 ; x = i ; x++) {
+      if (i % x === 0) {
+        console.log(x);
+        return false;
+      }
+    }
+    return true;
+  }
+  calcular() {
+    this.primos(this.numero);
   }
 
 }
